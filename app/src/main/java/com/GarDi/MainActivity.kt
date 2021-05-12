1b7d82a343cc08c3365482a443ec723511249dd1
Lukkah123 <h_lukka@hotmail.com>
2021-05-12 16:31:35 +0200

    added recycling button

--- app/src/main/java/com/GarDi/MainActivity.kt
+++ app/src/main/java/com/GarDi/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.GarDi
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,6 +16,7 @@ import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+        val button: Button = findViewById(R.id.button2)
+        button.setOnClickListener(View.OnClickListener {
+            val uri = "geo:447181.727,6209886.099?q=Recycling Center near me"
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
+        })
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         val fab: FloatingActionButton = findViewById(R.id.floatButton);
